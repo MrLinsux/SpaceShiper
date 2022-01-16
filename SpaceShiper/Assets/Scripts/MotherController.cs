5c829901e40c9d2bacd4b8f55e8db15a0065cf13
mrPlag <lego.teory@gmail.com>
2022-01-17 02:22:09 +0300

    add level selector base

--- SpaceShiper/Assets/Scripts/MotherController.cs
+++ SpaceShiper/Assets/Scripts/MotherController.cs
@@ -23,20 +23,6 @@ public class MotherController : MonoBehaviour
     }
     private int world = 0;
 
-    public int Level
-    {
-        get 
-        {
-            return level;
-        }
-        set
-        {
-            if (value >= 0)
-                level = value;
-        }
-    }
-    private int level = 0;
-
     public int Money
     {
         get
@@ -62,12 +48,53 @@ public class MotherController : MonoBehaviour
 
     public LevelButton[] levels;
 
+    public GameObject levelsPanel;
+
     private void Start()
     {
-        
+        Level[] levelsType = new Level[18];
+
+        for (int i = 0; i < 13; i++)
+            levelsType[i] = new Level(0, 3);
+
+        levelsType[13] = new Level(1, 0);
+
+        for (int i = 14; i < 18; i++)
+            levelsType[i] = new Level(2, 0);
+
+        levels = new LevelButton[18];
+
+        for (int i = 18; i < 18 + 18; i += 3)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                levels[i + j - 18] = levelsPanel.transform.GetChild(i + j).GetComponent<LevelButton>();
+                levels[i + j - 18].SetButton(world, (LevelButton.LevelStatus)levelsType[i + j - 18].type, levelsType[i + j - 18].stars);
+            }
+        }
+        for (int i = 22; i < 18 + 18; i += 3)
+        {
+            for (int j = 2; j >= 0; j--)
+            {
+                levels[i + j - 18] = levelsPanel.transform.GetChild(i + j).GetComponent<LevelButton>();
+                levels[i + j - 18].SetButton(world, (LevelButton.LevelStatus)levelsType[i + j - 18].type, levelsType[i + j - 18].stars);
+            }
+        }
+        if ((int)(Screen.currentResolution.width/Screen.currentResolution.height) == (int)(16/9))
+            levelsPanel.transform.localScale = new Vector3(0.85f, 0.85f, 1f);
     }
 
     [Serializable]
+    class Level
+    {
+        public int type = 0;
+        public int stars = 0;
+
+        public Level(int _type, int _stars)
+        {
+            type = _type; stars = _stars;
+        }
+    }
     class SaveData
     {
         public int money;
--- SpaceShiper/Assets/Scripts/UI/LevelButton.cs
+++ SpaceShiper/Assets/Scripts/UI/LevelButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,59 @@ using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour
 {
+    public enum LevelStatus { complete, active, close };
     public GameObject[] s;
     public int stars = 0;
     public int world = 0;
     public int level = 0;
-    public bool isComplete = false;
     public GameController controller;
 
-    public void SetButton(int _world, int _level)
+    public void SetButton(int _world, LevelStatus status, int stars)
     {
-        for(int i = 0; i < stars; i++)
+        world = _world;
+        level = Convert.ToInt32(this.name);
+        for (int i = 0; i < 3; i++)
         {
-            s[i].SetActive(true);
+            s[i].SetActive(false);
         }
 
-        world = _world; level = _level;
+        if (status == LevelStatus.active)
+        {
+            this.GetComponent<Image>().color = Color.yellow;
+            this.transform.localScale = new Vector3(1.1f, 1.1f, 1);
+
+            this.transform.GetChild(0).GetComponent<Text>().text = level.ToString();
+            this.GetComponent<Button>().interactable = false;
+        }
+        else if(status == LevelStatus.close)
+        {
+            var textColor = this.transform.GetChild(0).GetComponent<Text>().color;
+            this.transform.GetChild(0).GetComponent<Text>().text = "x";
+            textColor.a = 0.5f;
+            this.GetComponent<Button>().interactable = false;
+            var color = this.GetComponent<Image>().color;
+            color = Color.white;
+            color.a = 0.5f;
+        }
+        else if(status == LevelStatus.complete)
+        {
+            this.GetComponent<Button>().interactable = true;
+            for (int i = 0; i < stars; i++)
+            {
+                s[i].SetActive(true);
+            }
+
+            level = Convert.ToInt32(this.name);
+
+            this.transform.GetChild(0).GetComponent<Text>().text = level.ToString();
+            this.GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            Debug.LogError("Level " + level + " is not load!");
+            this.GetComponent<Image>().color = Color.red;
+            this.GetComponent<Button>().interactable = false;
+        }
     }
 
     public void LoadLevel()
@@ -30,17 +69,5 @@ public class LevelButton : MonoBehaviour
     private void Start()
     {
         controller = GameObject.Find("GameController").GetComponent<GameController>();
-        if(!isComplete)
-        {
-            this.GetComponent<Image>().color = Color.yellow;
-            for (int i = 0; i < s.Length; i++)
-            {
-                s[i].SetActive(false);
-            }
-        }
-        else
-        {
-
-        }
     }
 }
