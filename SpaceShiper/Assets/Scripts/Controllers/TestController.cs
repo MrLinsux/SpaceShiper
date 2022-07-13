9cc2201972c25b0dacda0eef3c5f0d4c1e2b2800
mrPlag <lego.teory@gmail.com>
2022-07-13 18:44:49 +0300

    add debugMenu

--- SpaceShiper/Assets/Scripts/Controllers/TestController.cs
+++ SpaceShiper/Assets/Scripts/Controllers/TestController.cs
@@ -5,28 +5,14 @@ using UnityEngine.Tilemaps;
 
 public class TestController : MonoBehaviour
 {
-    public Tilemap tilemap;
-    public Tile wall;
-    public Tile way;
-
-    // Start is called before the first frame update
     void Start()
     {
-        var pi = Mathf.PI;
-        var n = 1024;
-        for (int x = 0; x < 2 * n; x++)
-        {
-            var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            obj.transform.localScale = Vector3.one * 0.1f;
-            obj.name = x.ToString();
-            obj.transform.position = (2 * Mathf.Sqrt(2) * new Vector2(Mathf.Pow(Mathf.Cos(x * pi / n + pi / 4), 4), Mathf.Pow(Mathf.Sin(x * pi / n + pi / 4), 4)));
-        }
-
+        Debug.Log("Start");
     }
 
-    // Update is called once per frame
+    int time = 0;
     void Update()
     {
-        
+        Debug.Log("Time: " + (time++));
     }
 }
